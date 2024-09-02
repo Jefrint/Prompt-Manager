4811d997c37762c6fb435b713d2319232f673793
Jefrint <jefrintm@gmail.com>
2024-09-02 21:58:41 +0530

    added sign in signout

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,19 @@ import { useState,useEffect } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const nav = () => {
+   const isUserLoggedIn=true
+const [providers,setProviders]=useState(null);
+const [toggleDropdown,settoggleDropdown]=useState(false);
+useEffect(()=>{
+(
+  async()=>{
+    const response = await getProviders();
+    console.log(response)
+    setProviders(response)})()
+},[])
+ console.log(toggleDropdown)
+
+
   return (
     <nav className='flex-between w-full  mb-16 pt-3'>
     <Link href='/' className='flex gap-2  flex-center'>
@@ -17,7 +30,74 @@ const nav = () => {
         className='object-contain'
       />
       <p className='logo_text'>Promptopia</p>
-    </Link>
+      </Link>
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+         
+ <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
+            </Link>
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+            <Link href='/profile'>
+              <Image
+                src={'/assets/images/logo.svg'}
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+              />
+            </Link>
+          </div>
+        ):( <div>
+         { providers && Object.values(providers).map((providers)=>(
+          <button 
+          type='button'
+          key={providers.id}
+          onClick={()=> signIn(providers.id)}>
+
+          </button>
+         ))}
+
+
+        </div> )}
+
+       
+      </div>
+
+      <div className='sm:hidden flex relative'>
+        {isUserLoggedIn ? ( 
+          <div className='flex'>
+          <Image
+                src={'/assets/images/logo.svg'}
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+                onClick={()=> settoggleDropdown((prev)=> !prev) 
+                  
+                }
+              />
+        </div> ):(
+          <div>
+         { providers && Object.values(providers).map((providers)=>(
+          <button 
+          type='button'
+          key={providers.id}
+          onClick={()=> signIn(providers.id)}>
+Sign In
+          </button>
+         ))}
+
+
+        </div> 
+         )}
+
+
+        </div>
+    
     </nav>
   )
 }
